3bce624f5f70734354125f6123b65a896fc010ae
Zhengru Jiang <jzr98@qq.com>
2018-02-13 12:32:46 +0700

    delete something useless

--- freetype-demo.cpp
+++ freetype-demo.cpp
@@ -191,12 +191,6 @@ int main(){
     return 0;
 }
 
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
-{
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
 void InitWindow()
 {
     glfwInit();
